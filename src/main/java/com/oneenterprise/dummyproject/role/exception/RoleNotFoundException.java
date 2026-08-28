00245cf42cf1f196f31c61891e87eae3289a0ebd
MANO384 <palanimanojkumar924@gmail.com>
2026-08-28 16:57:12 +0530

    chore(role-service): remove duplicate legacy role package and clean repository

--- src/main/java/com/oneenterprise/dummyproject/role/exception/RoleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.oneenterprise.dummyproject.role.exception;
-
-public class RoleNotFoundException extends RuntimeException {
-    public RoleNotFoundException(String message) {
-        super(message);
-    }
-}
